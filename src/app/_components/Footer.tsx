d9d199dc7b6e618d7afff01e4d75c6d5c2e0e289
faisal004 <faisalhusain1320@gmail.com>
2024-10-27 09:58:37 +0530

    changes

---
+++ src/app/_components/Footer.tsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return ( <div>
+        hi
+    </div> );
+}
+ 
+export default Footer;
--- src/app/_components/Mill.tsx
+++ src/app/_components/Mill.tsx
@@ -1,13 +1,45 @@
+import Image from "next/image"
+
 const Mill = () => {
   return (
-    <div className=" h-[500px] bg-cover bg-no-repeat text-center bg-[url('/mill.png')] relative ">
-        <div className="bg-black/40 absolute inset-0">
-
-        </div>
+    <div className="flex flex-col">
+      <div className=" h-[500px] bg-cover bg-no-repeat text-center bg-[url('/mill.png')] relative ">
+        <div className="bg-black/40 absolute inset-0"></div>
         <div className="flex absolute top-20 text-left left-4 text-white font-bold text-4xl z-30">
-        TOGETHER, WE CAN BUILD A BETTER, AND MORE SUSTAINABLE WORKING WORLD.
-
+          TOGETHER, WE CAN BUILD A BETTER, AND MORE SUSTAINABLE WORKING WORLD.
         </div>
+      </div>
+      <div>
+        <div className="grid md:grid-cols-3 grid-cols-1 p-10 md:divide-x-4 md:divide-yellow-500 ">
+          <div className="flex flex-col p-5 space-y-5">
+            <div className="text-2xl font-bold">Experience</div>
+            <div className="text-base">
+              A healthier, more resilient, and innovative world by delivering
+              sustainable solutions that drive progress on ESG goals while
+              enhancing business performance.
+            </div>
+          </div>
+          <div className="flex flex-col p-5 space-y-5">
+            <div className="text-2xl font-bold">Learn</div>
+            <div className="text-base">
+              Latest design and development approaches, creating innovative and
+              Design2Delivery solutions to the fashion industry with a
+              sustainable ecoesystem in mind.
+            </div>
+          </div>
+          <div className="flex flex-col p-5 space-y-5">
+            <div className="text-2xl font-bold">Connect</div>
+            <div className="text-base">
+              Our deep technical expertise addresses the collections from
+              material science, product design, life cycle assessment, and
+              environmental and social impact.
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center justify-center my-10">
+        <Image src={"/image3.png"} alt="Image" height={"800"} width={"800"}/>
+      </div>
     </div>
   )
 }
