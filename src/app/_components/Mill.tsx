f6d74f655646dfe930e7da9985296734d966e613
faisal004 <faisalhusain1320@gmail.com>
2024-10-27 10:22:24 +0530

    changes

--- src/app/_components/Mill.tsx
+++ src/app/_components/Mill.tsx
@@ -46,10 +46,35 @@ const Mill = () => {
             <Image src={'/image2.png'} alt="image" height={400} width={400} />
           </div>
 
-            <div className="md:text-7xl text-4xl mt-5 md:mt-0 font-light tracking-widest  md:leading-[100px] h-full ">
+          <div className="md:text-7xl text-4xl mt-5 md:mt-0 font-light tracking-widest  md:leading-[100px] h-full ">
             CREATING A SUSTAINABLE ECOSYSTEM FOR FASHION.
-            </div>
-         
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center justify-center">
+        <div className="bg-gradient-to-r grid md:grid-cols-3 grid-cols-1 gap-20 from-[#d8e8dfd1] to-[#F2EEDE] p-10 mb-10 md:rounded-full rounded-md">
+          <div className="flex flex-col items-center justify-center gap-3">
+            <Image src={'/ICON-9.png'} alt="image" height={70} width={70} />
+            <div className="text-sm">SUSTAINABLE SOLUTIONS</div>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-3">
+            <Image src={'/ICON-9.png'} alt="image" height={70} width={70} />
+            <div className="text-sm">SUSTAINABLE SOLUTIONS</div>
+          </div>{' '}
+          <div className="flex flex-col items-center justify-center gap-3">
+            <Image src={'/ICON-9.png'} alt="image" height={70} width={70} />
+            <div className="text-sm">SUSTAINABLE SOLUTIONS</div>
+          </div>
+        </div>
+      </div>
+      <div className='flex flex-col items-center justify-center container mx-auto my-10 space-y-7'>
+        <div className='md:text-6xl text-4xl text-center text-stone-800 tracking-wide md:px-24  md:leading-[70px] '>A GLOBAL PERSPECTIVE SHAPED BY A DIVERSE MINDSET.</div>
+        <div className='text-center md:px-60 px-10 text-xl mt-1 md:leading-10'>
+          We believe that businesses and society must transform in order to
+          thrive towards a brighter future. While we commit to driving this
+          transformation for our clients to create a climate positive workflow,
+          we also believe that the work starts with our own commitment here at
+          ZER0.
         </div>
       </div>
     </div>
