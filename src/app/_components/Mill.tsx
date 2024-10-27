21beae3f8954e1a84361ea97a0c4c38fdd1531f8
faisal004 <faisalhusain1320@gmail.com>
2024-10-27 10:07:50 +0530

    changes

--- src/app/_components/Mill.tsx
+++ src/app/_components/Mill.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image"
+import Image from 'next/image'
 
 const Mill = () => {
   return (
@@ -38,7 +38,19 @@ const Mill = () => {
         </div>
       </div>
       <div className="flex items-center justify-center my-10">
-        <Image src={"/image3.png"} alt="Image" height={"800"} width={"800"}/>
+        <Image src={'/image3.png'} alt="Image" height={'800'} width={'800'} />
+      </div>
+      <div>
+        <div className="container mx-auto  grid md:grid-cols-2 grid-cols-1 p-20 ">
+          <div className="flex items-center justify-center">
+            <Image src={'/image2.png'} alt="image" height={400} width={400} />
+          </div>
+
+            <div className="md:text-7xl text-4xl mt-5 md:mt-0 font-light tracking-widest  md:leading-[100px] h-full ">
+            CREATING A SUSTAINABLE ECOSYSTEM FOR FASHION.
+            </div>
+         
+        </div>
       </div>
     </div>
   )
