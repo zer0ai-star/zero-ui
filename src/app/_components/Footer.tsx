0ea6ac0c75ceef7f4978e3e32b11f1da90aa64ca
faisal004 <faisalhusain1320@gmail.com>
2024-11-15 19:46:38 +0530

    social links added

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,5 +1,6 @@
-import { Mail, MapPin } from 'lucide-react'
+import { InstagramIcon, LinkedinIcon, Mail, MapPin } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
@@ -9,21 +10,38 @@ const Footer = () => {
           <Image src={'/logo.png'} alt="logo" height={120} width={120} />
         </div>
         <div className="absolute right-10">
-          <div className='flex gap-3 text-white md:text-xl text-md '>
-            <div className='text-right'>769 BROADWAY  <br />  #1106 MANHATTAN <br /> NY 10003 <br /> USA</div>
-            <MapPin className='h-10 w-10 text-white ' />
-          </div>
-          <div className='flex gap-3 text-white text-md'>
-            <div className='text-right w-full pr-10 mt-5'>
-                USA | INDIA
+          <div className="flex gap-3 text-white md:text-xl text-md ">
+            <div className="text-right">
+              769 BROADWAY <br /> #1106 MANHATTAN <br /> NY 10003 <br /> USA
             </div>
+            <MapPin className="h-10 w-10 text-white " />
           </div>
-          <div className='flex  text-white text-md gap-5 '>
-            <div className='text-right w-full  '>
-            info.zer0.ai 
-            </div>
-            <Mail className='h-6 w-6'/>
+          <div className="flex gap-3 text-white text-md">
+            <div className="text-right w-full pr-10 mt-5">USA | INDIA</div>
           </div>
+          <div className="flex  text-white text-md gap-5 ">
+            <div className="text-right w-full  ">info.zer0.ai</div>
+            <Mail className="h-6 w-6" />
+          </div>
+        </div>
+        <div className="absolute bottom-20 text-white flex items-center gap-5 ">
+          <Link
+            href={'https://www.linkedin.com/company/zer0innovativesolutions/'}
+            target="_blank"
+          >
+            <LinkedinIcon />
+          </Link>
+          <Link
+            href={
+              'https://www.instagram.com/zer0forplanet/profilecard/?igsh=Yml0Z3AwODJqd2tz'
+            }
+            target="_blank"
+          >
+            <InstagramIcon />
+          </Link>
+          <Link href={'https://forms.gle/EVx266HDexYv1XMn6'} target="_blank">
+            <Mail />
+          </Link>
         </div>
       </div>
     </div>
