240972a5bc3b17580090cf8db62fbdd2dcaa51b2
Faisal Husain <faisalhusain1320@gmail.com>
2025-01-21 14:09:20 +0530

    Revert "Update googleAnalytics.tsx"
    This reverts commit 964d2aa55f01e614d556b8ee1bdd00dcc55c53d7.

--- src/app/_components/googleAnalytics.tsx
+++ src/app/_components/googleAnalytics.tsx
@@ -4,14 +4,21 @@ import Script from 'next/script';
 const GoogleAnalytics = () => {
   return (
     <>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-EWP1CFS6Q6"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-      
-        gtag('config', 'G-EWP1CFS6Q6');
-      </script>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+      />
+
+      <Script id='' strategy='lazyOnload'>
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
+             
+              });
+          `}
+      </Script>
     </>
   );
 };
